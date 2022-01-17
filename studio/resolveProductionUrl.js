63b4d731b91204f03c69775fcfefbf48ff391177
danjonesdev <dannyjones360@gmail.com>
2022-01-17 19:50:56 +0000

    Minors

--- studio/resolveProductionUrl.js
+++ studio/resolveProductionUrl.js
@@ -5,5 +5,5 @@ export default function resolveProductionUrl(document) {
   let page = document._type;
   let slug = document?.slug?.current || "null";
 
-  return `http://localhost:3000/api/preview?secret=${previewSecret}&page=${page}&slug=${slug}`;
+  return `${process.env.SITE_URL}/api/preview?secret=${previewSecret}&page=${page}&slug=${slug}`;
 }
