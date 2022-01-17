0942fc19f1ede88720f767cd4a1a9d07df82ab75
danjonesdev <dannyjones360@gmail.com>
2022-01-17 17:51:03 +0000

    Minors

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  env: {
+    SITE_URL: process.env.SITE_URL,
+    MAILCHIMP_LIST_ID: process.env.MAILCHIMP_LIST_ID,
+    MAILCHIMP_API_KEY: process.env.MAILCHIMP_API_KEY,
+  },
+};
